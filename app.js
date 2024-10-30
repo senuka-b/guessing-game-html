aeb211b9388ddbe9a17392aafd3e01c0e79c42d2
yetimeh <senukabandara2006@gmail.com>
2024-10-30 10:50:59 +0530

    Create guessing game

---
+++ app.js
@@ -0,0 +1,63 @@
+let randomNumber = generateRandomNumber();
+let chances = 3;
+let won = false;
+
+function generateRandomNumber() {
+    return Math.floor((Math.random()*10)+1);
+}
+
+function restart() {
+    alert("Starting a new game!");
+
+    randomNumber = generateRandomNumber();
+    chances = 3;
+    won = false;
+    
+    document.getElementById("txtNumber").value = "";
+    document.getElementById("lblChancesLeft").innerHTML = `Chances left : ${chances}`;
+    document.getElementById("lblResult").innerHTML = "Waiting for you to guess... [New Game]";
+    document.getElementById("lblResult").className = "d-flex align-items-center justify-content-center pt-3 pb-4";
+}
+
+function guess() {
+
+    console.log("Random number: " + randomNumber);
+
+    let guessNumber = Number.parseInt(document.getElementById("txtNumber").value);
+    let chancesLeftElement = document.getElementById("lblChancesLeft");
+    let resultElement = document.getElementById("lblResult");
+
+    chances--;
+    chancesLeftElement.innerHTML = `Chances left: ${chances}`;
+
+    if (chances < 0) return restart(); 
+
+    if (chances > 0 || guessNumber == randomNumber) {
+
+        if (won) return restart();
+
+        if (guessNumber == randomNumber) {
+            resultElement.innerHTML = `You won! The number was ${randomNumber}`;
+            resultElement.className = "d-flex align-items-center justify-content-center pt-3 text-success pb-4 ";
+
+            won = true;
+            
+        } else if (guessNumber > randomNumber) {
+            resultElement.innerHTML = `Nope. Try again with a lower number!`;
+            resultElement.className = "d-flex align-items-center justify-content-center pt-3 text-danger pb-4"
+
+        } else {
+            resultElement.innerHTML = `Nope. Try again with a higher number!`
+            resultElement.className = "d-flex align-items-center justify-content-center pt-3 text-danger pb-4"
+        }
+
+        
+    
+    } else {
+        resultElement.innerHTML = `Game Over! The number was ${randomNumber} & you ran out of chances!`
+        resultElement.className = "d-flex align-items-center justify-content-center pt-3 text-danger pb-4"
+    }
+
+   
+
+}
